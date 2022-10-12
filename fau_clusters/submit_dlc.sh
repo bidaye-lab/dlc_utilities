307e958af452f8bd39d6afe99d73074dfe2f9320
Nico Spiller <32197051+nspiller@users.noreply.github.com>
2022-10-12 19:53:06 -0400

    Create submit_dlc.sh
    submit script

---
+++ fau_clusters/submit_dlc.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#SBATCH --partition=shortq7
+#SBATCH --gres=gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=20
+#SBATCH --output="%x.%j.out"
+#SBATCH --error="%x.%j.err"
+
+# SLURM submit script to run DLC with custom conda environment 
+# not via modules
+
+# print general slurm info
+echo "HPC: Start time is $(date) $(date +%s)"
+echo "HPC: SLURM_JOB_NAME is $SLURM_JOB_NAME"
+echo "HPC: SLURM_SUBMIT_DIR is $SLURM_SUBMIT_DIR"
+echo "HPC: SLURM_JOB_ID is $SLURM_JOB_ID"
+echo "HPC: SLURM_NODELIST is $SLURM_NODELIST"
+echo "HPC: SLURM_CPUS_PER_TASK is $SLURM_CPUS_PER_TASK"
+
+# activate conda env
+eval "$(conda shell.bash hook)"
+conda activate deeplabcut
+
+# execute task
+echo "HPC: Running DLC..."
+echo
+
+srun python $1 2>&1
+
+echo
+echo "HPC: ...DLC finished"
+echo "HPC: End time is $(date) $(date +%s)"
