0d425046ae33376ea088ed8aaf7bde747aede4ac
Nico Spiller <32197051+nspiller@users.noreply.github.com>
2023-02-01 16:36:43 -0500

    Changed bond length

--- 3d_visualization/csv2xyz.py
+++ 3d_visualization/csv2xyz.py
@@ -11,25 +11,27 @@ def run():
     
     # command line parser
     parser = argparse.ArgumentParser(
-        description='''Create xyz trajectory file from CSV''')
+        description='''Create xyz trajectory file(s) from CSV''')
     parser.add_argument('csv', help='Name of the CSV file')
     parser.add_argument('-s', '--split', metavar='S', 
-    help='Split output files to S frames per file. Default: 1400', default=1400, type=int)
-    # group = parser.add_mutually_exclusive_group()
-    # group.add_argument('-n', '--entry', metavar='N', type=int, default=1, 
-    #                    help='Replace values with nth entry (default: 1). To replace with mean of whole column, choose 0')
-    args = parser.parse_args()
-    csv = Path(args.csv)
-    split = args.split
-    # bx, by, bz = 1556.960000844912, -7245.893156816584, 9129.774962573414
+    help='Split output files to S frames per file. Select 0 for no splitting. Default: 1400', default=1400, type=int)
+    args = parser.parse_args(['P9-RightTurner-N4-3Dpose.csv'])
 
+    csv = Path(args.csv) # input CSV
+    split = args.split # number of frames for splitting
+    scl = 10 # scale for reasonable "bond lengths"
+
+    print('INFO reading file {}'.format(csv))
     df = pd.read_csv(csv) # read CSV into pandas dataframe
-    df = df * 100 # scale for reasonable "bond lengths"
+    print('INFO scaling distances by {}'.format(scl))
+    df = df * scl 
 
     col_x = [ i for i in df.columns if i.endswith('_x')]
-    points = [ i.rstrip('_x') for i in col_x ]
+    points = [ i.rstrip('x').rstrip('_') for i in col_x ]
     n = len(points) # number of "atoms"
+    print('INFO found {} points'.format(n))
 
+    print('INFO loading data ...')
     lines = []
     for i in df.index:
 
@@ -44,6 +46,7 @@ def run():
             l = '{} {} {} {}\n'.format(j, x, y, z)
             lines.append(l)
 
+        # TODO add ball
         # l = '{} {} {} {}\n'.format('Ball', x, y+300, z)
         # lines.append(l)
 
@@ -58,6 +61,7 @@ def run():
             if not i % len_blk:
                 out.close() # close previous file
                 fid += 1
+                print('INFO writing file {}'.format(xyz(fid)))
                 out = open(xyz(fid), 'w')
 
             out.write(l)
@@ -68,6 +72,7 @@ def run():
 
     else: # if 0, write one file
         xyz = csv.with_suffix('.xyz')
+        print('INFO writing file {}'.format(xyz))
         with open(xyz, 'w') as f:
             f.writelines(lines)
 
