2daccced5486742e39c822f70ca5fa1c8431f708
Jacob.Ryabinky@mpfi.org <mrjacobry@gmail.com>
2023-06-27 10:47:39 -0400

    Moved two functions to utils to clean up preprocess.py

--- unified_pipeline/preprocess.py
+++ unified_pipeline/preprocess.py
@@ -8,7 +8,6 @@ import pickle
 pickle.HIGHEST_PROTOCOL = 4
 import pandas as pd
 from pathlib import Path
-import shutil
 import utils
 
 def fix_point(df:pd.DataFrame, col_name: str = "F-TaG", n: int = 1) -> pd.DataFrame: 
@@ -78,39 +77,6 @@ def remove_cols(df:pd.DataFrame, start: str = "", end: str = "", ) -> pd.DataFra
 
     return df
 
-
-def create_file_path(path: Path, root: Path) -> Path:
-    """Create appropriate filename from a path for each data file in the format GenotypeFlynum-camName, e.g: for camA in the BPN dataset, for fly N1, filename: BPNN1-A
-
-    Parameters
-    ----------
-    path : Path
-        The path to the data file
-
-    Returns
-    -------
-    Path 
-        Path with file name in the format GenotypeFlynum-camName
-    """
-    
-    """
-    Directory structure example:
-
-    BallSystem_RawData \ <Genotype info> \ <0+ Subdirectories> \ <N1, N2...Nx> \ <Ball, SS> \ <Video & DLC files>
-
-    Camera name - directly from DLC generated file names
-    Fly number - parent of parent of DLC file
-    Genotype - Child of root
-    """
-    path_rel = path.relative_to(root)
-
-
-    cam_name = str(path.name)[0]
-    fly_num = str(path.parent.parent.name).strip()
-    genotype = str(path_rel.parts[0]).strip().replace("-", "").replace("_", "")
-    file_name = genotype + fly_num + "-" + cam_name
-    return path.with_name(file_name).with_suffix(path.suffix)
-
 def df2hdf(df: pd.DataFrame, path: Path, root: Path = Path(r'\\mpfi.org\public\sb-lab\BallSystem_RawData')) -> None:
     """Convert pandas DF provided to hdf format and save with proper name format
 
@@ -123,7 +89,7 @@ def df2hdf(df: pd.DataFrame, path: Path, root: Path = Path(r'\\mpfi.org\public\s
     """
     # Create new file name
     try:
-        new_path = create_file_path(path,root)
+        new_path = utils.create_file_path(path,root)
     except ValueError:
         print("[ERROR] Incorrect root.\nYour root path does not match with the parent directory provided, please make sure that you provided the correct root. \
         \nThe root should be the beginning of your parent directory path up to the folder containing raw data, e.g `\mpfi.org\public\sb-lab\BallSystem_RawData`\n")
@@ -137,53 +103,6 @@ def df2hdf(df: pd.DataFrame, path: Path, root: Path = Path(r'\\mpfi.org\public\s
     print(f"[INFO]: Writing to file {hdf}")
     df.to_hdf(hdf, key='df_with_missing', mode='w')
 
-def traverse_dirs(directory_structure: dict, path: Path = Path('')) -> None:
-    """Traverse the directory dict structure and generate analagous file structure
-
-    All directories are dicts but files are represented with the key 'files' and a list of either file names (with extension) or the full path to an existing file.
-    If the full path is provided, then the existing file will be moved from that location to the location specified in the dictionary structure.
-
-    Parameters
-    ----------
-    directory_structure : dict
-        File structure represented as a dictionary. 
-    path : Path, optional
-        Path to parent directory. The dictionary file structure will be generated such that path/<dict structure>, by default Path('')
-    """
-    for parent, child in directory_structure.items():
-        if isinstance(child, dict):
-            newpath = (path / parent)
-            if not newpath.exists():
-                print(f"[INFO] Creating new directory {newpath}")
-                newpath.mkdir()
-                traverse_dirs(child, newpath) # recursively call to traverse all subdirs
-            else:
-                print(f"[WARNING] Skipping creating {newpath} because it already exists")
-        elif parent == 'filesmv' and child:
-            for file in child:
-                if isinstance(file, Path):
-                    filepath = path / file.name
-                    if not filepath.exists():
-                        print(f"[INFO] Moving file {file} to {filepath}")
-                        file.rename(filepath) # if file path entered, move the existing file here
-                else:
-                    print(f"[WARNING] Skipping {file}, all files in `filesmv` should be paths")
-        elif parent == 'filescp' and child:
-            for file in child:
-                if isinstance(file, Path):
-                    filepath = path / file.name
-                    if not filepath.exists():
-                        print(f"[INFO] Copying file {file} to {filepath}")
-                        shutil.copy(file, filepath)
-                else:
-                    print(f"[WARNING] Skipping {file}, all files in `filescp` should be paths")
-        elif parent == 'filesmk' and child:
-            for file in child:
-                filepath = path / file
-                if not filepath.exists():
-                    print(f"[INFO] Creating new file at: {filepath}")
-                    # if only file name entered, create at location
-                    filepath.touch()
 
 def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration: Path, p_detection: Path, p_anipose_config: Path, structure:dict={}) -> None:
     """Generate the necessary anipose file structure given a parent path and a file structure
@@ -248,4 +167,4 @@ def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration: Path
         }
     }
 
-    traverse_dirs(structure, parent_dir)
+    utils.traverse_dirs(structure, parent_dir)
--- unified_pipeline/utils.py
+++ unified_pipeline/utils.py
@@ -6,6 +6,7 @@ __author__ = "Jacob Ryabinky"
 
 import yaml
 from pathlib import Path
+import shutil
 import pandas as pd
 
 
@@ -50,3 +51,83 @@ def find_nx_dirs(parent_dir: Path) -> list:
         if parent.name != 'project':
             dirs.append(parent)
     return dirs
+
+def create_file_path(path: Path, root: Path) -> Path:
+    """Create appropriate filename from a path for each data file in the format GenotypeFlynum-camName, e.g: for camA in the BPN dataset, for fly N1, filename: BPNN1-A
+
+    Parameters
+    ----------
+    path : Path
+        The path to the data file
+
+    Returns
+    -------
+    Path 
+        Path with file name in the format GenotypeFlynum-camName
+    """
+    
+    """
+    Directory structure example:
+
+    BallSystem_RawData \ <Genotype info> \ <0+ Subdirectories> \ <N1, N2...Nx> \ <Ball, SS> \ <Video & DLC files>
+
+    Camera name - directly from DLC generated file names
+    Fly number - parent of parent of DLC file
+    Genotype - Child of root
+    """
+    path_rel = path.relative_to(root)
+
+
+    cam_name = str(path.name)[0]
+    fly_num = str(path.parent.parent.name).strip()
+    genotype = str(path_rel.parts[0]).strip().replace("-", "").replace("_", "")
+    file_name = genotype + fly_num + "-" + cam_name
+    return path.with_name(file_name).with_suffix(path.suffix)
+
+def traverse_dirs(directory_structure: dict, path: Path = Path('')) -> None:
+    """Traverse the directory dict structure and generate analagous file structure
+
+    All directories are dicts but files are represented with the key 'files' and a list of either file names (with extension) or the full path to an existing file.
+    If the full path is provided, then the existing file will be moved from that location to the location specified in the dictionary structure.
+
+    Parameters
+    ----------
+    directory_structure : dict
+        File structure represented as a dictionary. 
+    path : Path, optional
+        Path to parent directory. The dictionary file structure will be generated such that path/<dict structure>, by default Path('')
+    """
+    for parent, child in directory_structure.items():
+        if isinstance(child, dict):
+            newpath = (path / parent)
+            if not newpath.exists():
+                print(f"[INFO] Creating new directory {newpath}")
+                newpath.mkdir()
+                traverse_dirs(child, newpath) # recursively call to traverse all subdirs
+            else:
+                print(f"[WARNING] Skipping creating {newpath} because it already exists")
+        elif parent == 'filesmv' and child:
+            for file in child:
+                if isinstance(file, Path):
+                    filepath = path / file.name
+                    if not filepath.exists():
+                        print(f"[INFO] Moving file {file} to {filepath}")
+                        file.rename(filepath) # if file path entered, move the existing file here
+                else:
+                    print(f"[WARNING] Skipping {file}, all files in `filesmv` should be paths")
+        elif parent == 'filescp' and child:
+            for file in child:
+                if isinstance(file, Path):
+                    filepath = path / file.name
+                    if not filepath.exists():
+                        print(f"[INFO] Copying file {file} to {filepath}")
+                        shutil.copy(file, filepath)
+                else:
+                    print(f"[WARNING] Skipping {file}, all files in `filescp` should be paths")
+        elif parent == 'filesmk' and child:
+            for file in child:
+                filepath = path / file
+                if not filepath.exists():
+                    print(f"[INFO] Creating new file at: {filepath}")
+                    # if only file name entered, create at location
+                    filepath.touch()
