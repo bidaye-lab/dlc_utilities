9f6cacd21927c033de15d0899f7852e1f048a1ca
Jacob.Ryabinky@mpfi.org <mrjacobry@gmail.com>
2023-07-18 14:50:14 -0400

    updated documentation for preprocess.py

--- unified_pipeline/preprocess.py
+++ unified_pipeline/preprocess.py
@@ -26,6 +26,8 @@ def create_file_name(path: Path, root: Path) -> Path:
     ----------
     path : Path
         The path to the data file
+    root : Path
+        The path to the root directory
 
     Returns
     -------
@@ -53,9 +55,9 @@ def create_file_name(path: Path, root: Path) -> Path:
 
 
 def to_dt(date_string: str, time: bool = False) -> datetime:
-    match="%m%d%Y"
+    match="%m%d%Y" # only date
     if time:
-        match = "%m%d%Y%H%M%S"
+        match = "%m%d%Y%H%M%S" # if the DT string contains a time as well
     return datetime.strptime(date_string, match)
 
 def get_date_time(p_project_dir: Path) -> datetime:
@@ -82,17 +84,17 @@ def get_calibration_type(p_calibration_target: Path, p_project_dir: Path):
 def get_anipose_calibration_files(p_calibration_target: Path, p_calib_timeline: Path, p_project_dir: Path) -> list:
     p_calibration_files = ""
    
-    calibration_timeline = utils.load_config(p_calib_timeline)
+    calibration_timeline = utils.load_config(p_calib_timeline) # specifies which calibration to use based on the timestamp on filename
 
    
-    project_date = get_date_time(p_project_dir)
+    project_date = get_date_time(p_project_dir) # gets the datetime string from filename
 
     for path, daterange in calibration_timeline.items():
-        start = daterange.split("-")[0].strip()
-        end =  daterange.split("-")[1].strip()
+        start = daterange.split("-")[0].strip() # Start datetime
+        end =  daterange.split("-")[1].strip() # end datetime
         dt_start = to_dt(start)
         dt_end = to_dt(end)
-        dt_daterange = DateTimeRange(dt_start, dt_end)
+        dt_daterange = DateTimeRange(dt_start, dt_end) # Create a date range (this just starts and ends at midnight since the time doesn't matter)
         if project_date in dt_daterange: # determine if the project falls in the date range
             p_calibration_files = Path(path)
             break
@@ -106,9 +108,11 @@ def get_anipose_calibration_files(p_calibration_target: Path, p_calib_timeline: 
         p_detection_pickle = next(p_calibration_files.glob('**/detections.pickle'))
         p_calibration_toml = next(p_calibration_files.glob('**/calibration.toml')) 
         if calibration_type == 'board':
+            # Board calibration needs both files
             output_files.append(p_detection_pickle)
             output_files.append(p_calibration_toml)
         elif calibration_type == 'fly':
+            # Fly-based calibration only requires detections.pickle
             output_files.append(p_detection_pickle)
         else:
             logging.error(f"Invalid calibration type or calibration type not specified in `{p_calibration_target}")
@@ -175,24 +179,37 @@ def remove_cols(df:pd.DataFrame, start: str = "", end: str = "", ) -> pd.DataFra
 
     # filter columns based on beginning of name
     if start:
-        cols = df.loc[ :, df.loc[1, :].apply(lambda x: str(x).startswith(start)) ].columns
+        cols = df.loc[ :, df.loc[1, :].apply(lambda x: str(x).startswith(start)) ].columns # find cols with the particular start string
         df = df.drop(columns=cols)
         logging.info(' removed {} columns starting with {}'.format(len(cols), start))
 
     # filter columns based on end of name
     if end:
-        cols = df.loc[ :, df.loc[1, :].apply(lambda x: str(x).endswith(end)) ].columns
+        cols = df.loc[ :, df.loc[1, :].apply(lambda x: str(x).endswith(end)) ].columns # find cols with the particular end string
         df = df.loc[:, cols]
         logging.info(' removed {} columns ending with {}'.format(len(cols), end))
 
     return df
 
 def clean_dfs(p_csv: Path) -> pd.DataFrame:
+    """Run any functions that clean the raw data. Any new cleaning steps can be added here.
+
+    Parameters
+    ----------
+    p_csv : Path
+        path to a CSV file that will be processed.
+
+    Returns
+    -------
+    pd.DataFrame
+        Processed CSV as a DF
+    """
     logging.info(f"Processing {p_csv.name}")
     csv_df = utils.load_csv_as_df(p_csv)
 
     # Fix points
     logging.info(" Running `Fix points` preprocessing...")
+    # Columns which will have points fixed, add/remove to change which cols processed
     col_names = [
         'R-F-ThC', 'R-M-ThC', 'R-H-ThC',
         'L-F-ThC', 'L-M-ThC', 'L-H-ThC', 
@@ -206,7 +223,7 @@ def clean_dfs(p_csv: Path) -> pd.DataFrame:
 
     # Remove cols 
     logging.info("Running `Remove cols` preprocessing...")
-    camName = p_csv.name[0]
+    camName = p_csv.name[0] # Camera letter name
     start = ''
     end = ''
     if camName == 'B':
@@ -220,18 +237,22 @@ def clean_dfs(p_csv: Path) -> pd.DataFrame:
     return csv_df
 
 def df2hdf(df: pd.DataFrame, csv_path: Path, write_path: Path, root: Path = root) -> None:
-    """Convert pandas DF provided to hdf format and save with proper name format
+    """Convert pandas DF provided to hdf format and save with proper name format 
 
     Parameters
     ----------
     df : pd.DataFrame
-        Preprocessed pandas dataframe representing DLC CSV data
-    path : Path
-        Path to the original CSV file
+        DF representing DLC data
+    csv_path : Path
+        Path to original CSV from which DF was generated
+    write_path : Path
+        Path to which HDF will be written
+    root : Path, optional
+        Root directory, by default root
     """
     # Create new file name
     try:
-        file_name = create_file_name(csv_path,root)
+        file_name = create_file_name(csv_path,root) # get filename from csv path in the form GenotypeFlynum-camName
     except ValueError:
         logging.critical("Incorrect root.\nYour root path does not match with the parent directory provided, please make sure that you provided the correct root. \
         \nThe root should be the beginning of your parent directory path up to the folder containing raw data, e.g `\mpfi.org\public\sb-lab\BallSystem_RawData`\n")
@@ -255,8 +276,10 @@ def traverse_dirs(directory_structure: dict, parent_dir: Path, path: Path = Path
     ----------
     directory_structure : dict
         File structure represented as a dictionary. 
+    parent_dir : Path
+        Path to parent directory. The dictionary file structure will be generated such that path/<dict structure>
     path : Path, optional
-        Path to parent directory. The dictionary file structure will be generated such that path/<dict structure>, by default Path('')
+        Used to call function recursively.
     """
     for parent, child in directory_structure.items(): # loop over the directory structure
         if isinstance(child, dict): # dict is a directory, create dir and then call recursively
@@ -318,12 +341,14 @@ def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration_targe
         Parent directory. This is where anipose folder will be placed
     p_network_cfg: Path 
         File path to the config file containing DLC model paths
-    p_calibration: Path
-        File path to calibration.toml
-    p_detection: Path
-        File path to detections.pickle
-    p_anipose_config:
-        File path to config.toml 
+    p_calibration_target: Path
+        File path to calibration target config file
+    p_calibration_timeline: Path
+        File path to calibration timeline config file
+    preprocessed_dfs: list
+        List of tuples in the form (processed_df, csv_path)
+    p_gcam_dummy: Path
+        filepath to the gcam dummy file
     structure : dict, optional
         The file structure represented as a dictionary, by default {}. If left blank, default will be used. The default will be the minimum required for anipose.
 
@@ -361,21 +386,23 @@ def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration_targe
         for file in ball_folder.glob('*.csv'): # Find all .csv files 
             if not genotype:
                 genotype = utils.get_genotype(file, root) # get genotype for G-cam dummy file 
+            # only process the filtered CSVs
             if 'filtered' in file.name: # TODO: change to check for model name and cam as well, i.e make sure that only grabbing files which match the currently set networks
                 logging.info(f"Found filtered CSV file {file}")
                 csv_files.append(file)
-        gcam = (p_gcam_dummy, f"{genotype}{folder.name}-G.h5")
+        gcam = (p_gcam_dummy, f"{genotype}{folder.name}-G.h5") # Create the gcam dummy file name by filling in the genotype and fly number
+        # Structure for the anipose `project` folder
         project[folder.name] = {
             'pose-2d': {
-                'filescv': preprocessed_dfs,
-                'filescp': [gcam]
+                'filescv': preprocessed_dfs, # will convert the DFs to HDF
+                'filescp': [gcam] # will copy the gcam file
             },
             'videos-raw':{}
         }
     
     # Get network set name
     cfg = utils.load_config(p_network_cfg)
-    network_set_name = cfg['Ball']['name']
+    network_set_name = cfg['Ball']['name'] # network set for ball
     logging.info(f"Using network set name {network_set_name}")
     if not structure:
         logging.info("Using default anipose file structure")
@@ -384,9 +411,9 @@ def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration_targe
         'anipose': {    
             'Ball': {
                 f'{network_set_name}': {
-                    'calibration':{'filescp':calibration_files},
-                    'project': project, # N1-Nx
-                    'filescp':[(p_anipose_config, 'config.toml')]
+                    'calibration':{'filescp':calibration_files}, # will copy calibration files
+                    'project': project, # N1-Nx 
+                    'filescp':[(p_anipose_config, 'config.toml')] # Will copy the anipose config as `config.toml`
                 },
             },
             'SS': {},
@@ -396,12 +423,24 @@ def gen_anipose_files(parent_dir: Path, p_network_cfg: Path, p_calibration_targe
     traverse_dirs(structure, parent_dir, parent_dir)
 
 def run_preprocessing(videos: Path, p_networks: Path, p_common_files: Path = Path(r"./common_files") ):
+    """Runs preprocessing on all CSV files generated by DLC in provided path
+
+    Parameters
+    ----------
+    videos : Path
+        Path to folder containing videos (doesn't have to be direct parent)
+    p_networks : Path
+        Path to network config files for DLC
+    p_common_files : Path, optional
+        Path to the common files folder, contains all config files, by default Path(r"./common_files")
+    """
+    #TODO: get network name from common files like everything else
     error = False
 
     # find all the CSVs that DLC generated
-    processed_dirs = {}
+    processed_dirs = {} # Will contain a dictionary with the filepath: list of tuples in form (csv_df, p_csv)
     for p_csv in utils.get_csvs(videos): # get all CSVs
-        parent_dir = p_csv.parent.parent.parent
+        parent_dir = p_csv.parent.parent.parent # The directory holding all data for that particular experiment, i.e parent of nx dir
 
         # Pick current generated, filtered CSV 
         
@@ -447,5 +486,5 @@ def run_preprocessing(videos: Path, p_networks: Path, p_common_files: Path = Pat
                 print(f"[WARNING] Skipped anipose generation for {parent_dir}")
         print('Finished preprocessing...')
     else:
-        print("Terminated due to error.")
+        print("Terminated due to error...")
 
