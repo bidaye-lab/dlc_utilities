da6dfcfedc87f1b93f358cd3d34e51ec64f829ef
Divya S Kumar <sthanukumard@mpfi.org>
2025-06-07 21:54:05 -0400

    fixed camG file renaming

--- unified_pipeline/src/file_tools.py
+++ unified_pipeline/src/file_tools.py
@@ -9,6 +9,7 @@ import yaml
 from pathlib import Path
 import pandas as pd
 
+
 def load_config(path: str):
     """Load config yml as dict.
 
@@ -20,21 +21,21 @@ def load_config(path: str):
     Returns
     -------
     config: dict
-        dictionary 
+        dictionary
     """
 
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         cfg = yaml.safe_load(f)
-
 
     return cfg
 
+
 def load_csv_as_df(csv: Path) -> pd.DataFrame:
-    
+
     # READS AS MULTI-INDEXEDS == does not work with current data preprocess methods
 
     df = pd.read_csv(csv, index_col=0, header=[0, 1, 2])
-    df.columns.set_levels([df.columns[0][0]], level='scorer')
+    df.columns.set_levels([df.columns[0][0]], level="scorer")
     return df
 
     # OLD CODE FOR FLAT READING
@@ -48,23 +49,27 @@ def get_csvs(path: Path) -> list:
         csv_paths.append(csv)
     return csv_paths
 
+
 def backup_file(path: Path) -> None:
-    backup = Path(str(path) + '_backup')
-    path.replace(backup) 
-    logging.info(f'backup file saved to {backup}')
+    backup = Path(str(path) + "_backup")
+    path.replace(backup)
+    logging.info(f"backup file saved to {backup}")
+
 
 def find_nx_dirs(parent_dir: Path) -> list:
     dirs = []
-    for n1 in parent_dir.glob('**/N1'):
+    for n1 in parent_dir.glob("**/N1"):
         parent = n1.parent
-        if parent.name != 'project':
+        if parent.name != "project":
             dirs.append(parent)
     return dirs
+
 
 def get_genotype(path: Path, root: Path) -> str:
     # Helper function to get genotype for G-cam dummy file
     path_rel = path.relative_to(root)
 
-    genotype = str(path_rel.parts[0]).strip().replace("-", "").replace("_","")
+    # genotype = str(path_rel.parts[0]).strip().replace("-", "").replace("_","")
+    genotype = root.parts[-1] + "_" + path_rel.parts[0]
+    print(genotype)
     return genotype
-
